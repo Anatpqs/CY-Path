a293fe630255fa0b0f64a25feea715f4afe9e810
JulianGssl <120329475+JulianGssl@users.noreply.github.com>
2023-05-27 17:11:43 +0200

    Update and rename Niveau.java to Level.java

---
+++ Level.java
@@ -0,0 +1,71 @@
+package application;
+/**
+ * <p> Class Level used to create levels
+ * @author Julian
+ *
+ */
+public class Level {
+    private String name;
+    private int[][] grid;
+    private int row;
+    private int column;
+    private int score;
+    
+    /**
+     * <p> Method that create a level with his name, row, column.
+     * 
+     * @param name : name of the level
+     * @param row : number of row in the level
+     * @param column : number of column in the level
+     */
+    public Level(String name, int row, int column) {
+        this.name = name;
+        this.row = row;
+        this.column = column;
+        this.grid = new int[row][column];
+        this.score = 0;
+    }
+    /**
+     * <p> Method that set the value of the selected tile.
+     * 
+     * @param value : new value of the tile
+     */
+    public void setTile(int row, int column, int value) {
+        grid[row][column] = value;
+    }
+    /**
+     * 
+     * @param i : row index of the tile 
+     * @param j : column index of the tile
+     * @return : return the changed grid of the level with the new value of the changed tile
+     */
+    public int getTile(int i, int j) {
+        if (i >= 0 && i < row && j >= 0 && j < column) {
+            return grid[i][j];
+        } else {
+            throw new IndexOutOfBoundsException("Coordonnées de Tuile invalides.");
+        }
+    }
+    public int[][] getGrid() {
+        return grid;
+    }
+    public String getName() {
+        return name;
+    }
+    
+    public int getRow() {
+        return row;
+    }
+    
+    public int getColumn() {
+        return column;
+    }
+    
+    public int getScore() {
+        return score;
+    }
+    
+    public void setScore(int score) {
+        this.score = score;
+    }    
+}
